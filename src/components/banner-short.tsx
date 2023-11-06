b99673f44ed8a9431f29745b501dc482362527eb
yavchris07 <wilondjayav27@gmail.com>
2023-11-06 15:18:14 +0100

    commit one

---
+++ src/components/banner-short.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/banner1.scss'
+
+interface titleProps{
+  title:string
+}
+
+function BannerShort({title}:titleProps) {
+  return (
+    <section className='banner1'>
+       <div className='cover-short'>
+        <h1>{title}</h1>
+        {/* le logo de l fondtion */}
+        <p>ELIKIA FONDATION</p>
+       </div>
+    </section>
+  );
+}
+
+export default BannerShort;
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import BannerShort from '../components/banner-short';
+ 
 
 export default function About() {
   return (
-    <div>About</div>
+    <> 
+    <BannerShort title='About' />
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptatem, cum odit laudantium incidunt amet itaque labore rem inventore vel natus facilis? Tenetur id dolorem ipsum, nobis suscipit quae quidem!</p>
+    </>
   )
 }
